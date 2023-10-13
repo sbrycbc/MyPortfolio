d88eb44ec13f5667b4143836bfd3c7afad34ffa3
sbrycbc <sabriyecbc@gmail.com>
2023-10-13 20:05:20 +0200

    added navbar for menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import React from 'react';
+import {
+  FaBars,
+  FaTimes,
+  /* FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaLinkedinIn, */
+} from 'react-icons/fa';
+import Logo from '../assets/sbry.png'
 
 const Navbar = () => {
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'></div>
+    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div>
+          <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
+        </div>
+
+{/* ----------- menu ------------  */}
+        <div>
+          <ul className='flex'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+
+{/* -------- Hamburger menu ------- */}
+        <div className='hidden'>
+            <FaBars />
+        </div>
+
+
+{/* ---------- Mobile menu -------- */}      
+         <ul className='hidden'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skills</li>
+            <li>Work</li>
+            <li>Contact</li>
+          </ul>
+
+
+{/* --------- Social icons -------- */}
+          <div></div>
+        
+    </div>
   )
 }
 
