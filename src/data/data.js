d4fb35decafc2729fdbf8fe38395c48579aade46
sbrycbc <sabriyecbc@gmail.com>
2023-10-16 00:25:38 +0200

    added project fotos and work pages...

---
+++ src/data/data.js
@@ -0,0 +1,39 @@
+import Beaches from '../assets/projects/Beaches.png';
+import DelishDeliveries from '../assets/projects/DelishDeliveries.png';
+import Employess from '../assets/projects/Employess.png';
+import Pokemon from '../assets/projects/terminal-foto.png';
+
+
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Beaches,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:DelishDeliveries,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:Employess,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:Pokemon,
+        github:"",
+        live:"",
+    },
+
+
+]
