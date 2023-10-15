57ceab145679274ccad71bee8cc4f6de009b44ec
sbrycbc <sabriyecbc@gmail.com>
2023-10-15 20:56:57 +0200

    added About page...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         {/* ----------- Container ------------  */}
 
         <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
-            <p className='text-[#6fc2b0]'>Hi, my name is</p>
+            <p className='text-pink-300'>Hi, my name is</p>
             <h1 className='text-4xl sm:text-5xl font-bold text-[#ccd6f6]'>Sabriye Cebeci</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I am a mother 😉 a wife 😍 a runner 🏃‍♂️ a cat lover 🐈‍⬛ with a big sense of humor ☕. Always willing to learn and open to innovations 🌏. Excited about working on ambitious projects 🎗 with positive people. For more info, feel free to contact me🍀🎶🍀</p>
