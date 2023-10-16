00ef569c5a5d6714404cfb29033870b315bcb6d4
sbrycbc <sabriyecbc@gmail.com>
2023-10-16 21:48:13 +0200

    updated button in Work page...

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -3,8 +3,8 @@ import { data } from "../data/data.js";
 
 const Work = () => {
   // projects file
-  const project = data;
-  //setProject(data);
+  const projects = data;
+
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
@@ -15,10 +15,9 @@ const Work = () => {
           <p className="py-6">Check out some of my recent work</p>
         </div>
 
-        {/* container for projects */}
+        {/* Container for projects */}
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {/* Gird Item */}
-          {project.map((item, index) => (
+          {projects.map((item, index) => (
             <div
               key={index}
               style={{ backgroundImage: `url(${item.image})` }}
@@ -26,12 +25,12 @@ const Work = () => {
               flex justify-center text-center items-center mx-auto content-div "
             >
               {/* Hover effect for images */}
-              <div className="opacity-0 group-hover:opacity-100 ">
-                <span className="text-2xl font bold text-black tracking-wider ">
+              <div className="opacity-0 group-hover:opacity-100">
+                <span className="text-2xl font-bold text-black tracking-wider">
                   {item.name}
                 </span>
-                <div className="pt-8 text-center ">
-                  {/* eslint-disable-next-line */}
+                <div className="pt-8 text-center">
+                  {/* Render the "Code" button regardless of live availability */}
                   <a href={item.github} target="_blank">
                     <button
                       className="text-center rounded-lg px-4 py-3 m-2
@@ -40,15 +39,17 @@ const Work = () => {
                       Code
                     </button>
                   </a>
-                  {/* eslint-disable-next-line */}
-                  <a href={item.live} target="_blank">
-                    <button
-                      className="text-center rounded-lg px-4 py-3 m-2
-                       bg-white text-gray-700 font-bold text-lg"
-                    >
-                      Live
-                    </button>
-                  </a>
+                  {/* Render the "Live" button if the 'live' URL exists */}
+                  {item.live && (
+                    <a href={item.live} target="_blank">
+                      <button
+                        className="text-center rounded-lg px-4 py-3 m-2
+                         bg-white text-gray-700 font-bold text-lg"
+                      >
+                        Live
+                      </button>
+                    </a>
+                  )}
                 </div>
               </div>
             </div>
