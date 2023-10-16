3211430ab568107462b1a5a4b45c1084eb17b904
sbrycbc <sabriyecbc@gmail.com>
2023-10-16 21:36:52 +0200

    updated fotos and data.js  for work page

--- src/data/data.js
+++ src/data/data.js
@@ -2,6 +2,8 @@ import Beaches from '../assets/projects/Beaches.png';
 import DelishDeliveries from '../assets/projects/DelishDeliveries.png';
 import Employess from '../assets/projects/Employess.png';
 import Pokemon from '../assets/projects/terminal-foto.png';
+import FotoFrend from '../assets/projects/FotoFrend.png';
+import EadShop from '../assets/projects/ead-shop.png';
 
 
 
@@ -17,23 +19,35 @@ export const data=[
         id:2,
         name:"React JS Application",
         image:DelishDeliveries,
-        github:"",
-        live:"",
+        github:"https://github.com/sbrycbc/delish-deliveries",
+        live:"https://delish-deliveries.netlify.app/home",
     },
     {
         id:3,
         name:"React JS Application",
         image:Employess,
-        github:"",
-        live:"",
+        github:"https://github.com/sbrycbc/employee-management-system",
+        live:"https://employee-management-system-phi.vercel.app/",
     },
     {
         id:4,
         name:"React JS Application",
         image:Pokemon,
-        github:"",
-        live:"",
+        github:"https://github.com/sbrycbc/pokemon-game",
+      
     },
-
-
+    {
+        id:5,
+        name:"React JS Application",
+        image:EadShop,
+        github:"https://github.com/sbrycbc/sbry-first-project",
+        live:"https://sbrycbc.github.io/sbry-first-project/",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:FotoFrend,
+        github:"https://github.com/sbrycbc/foto_friend",
+        
+    },
 ]
