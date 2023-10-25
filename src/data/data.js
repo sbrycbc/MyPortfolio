96179e33abe0d56fd3f22da12bb68ef707bcf0c4
sbrycbc <sabriyecbc@gmail.com>
2023-10-25 21:48:20 +0200

    updated data.name for work page...

--- src/data/data.js
+++ src/data/data.js
@@ -10,42 +10,42 @@ import EadShop from '../assets/projects/ead-shop.png';
 export const data=[
     {
         id:1,
-        name:"React JS Application",
+        name:"Travel Website with Tailwind-css",
         image:Beaches,
         github:"https://github.com/sbrycbc/travel-website-using-tailwind-css/",
         live:"https://travel-website-using-tailwind-css.vercel.app/",
     },
     {
         id:2,
-        name:"React JS Application",
+        name:"Online Food Ordering app with Redux Toolkit",
         image:DelishDeliveries,
         github:"https://github.com/sbrycbc/delish-deliveries",
         live:"https://delish-deliveries.netlify.app/home",
     },
     {
         id:3,
-        name:"React JS Application",
+        name:"Employee Management System with React.js",
         image:Employess,
         github:"https://github.com/sbrycbc/employee-management-system",
         live:"https://employee-management-system-phi.vercel.app/",
     },
     {
         id:4,
-        name:"React JS Application",
+        name:"Pokemon Fight Game in the Terminal",
         image:Pokemon,
         github:"https://github.com/sbrycbc/pokemon-game",
       
     },
     {
         id:5,
-        name:"React JS Application",
+        name:"EadShop Website",
         image:EadShop,
         github:"https://github.com/sbrycbc/sbry-first-project",
         live:"https://sbrycbc.github.io/sbry-first-project/",
     },
     {
         id:6,
-        name:"React JS Application",
+        name:"Online Photo Gallery App with Expressjs",
         image:FotoFrend,
         github:"https://github.com/sbrycbc/foto_friend",
         
