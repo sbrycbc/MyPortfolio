a20d241f658b7a323adda179630d6e7402130bdc
sbrycbc <sabriyecbc@gmail.com>
2023-12-06 13:44:00 +0100

    updated about-page image...

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,29 +1,39 @@
 import React from "react";
+import ProfilFoto from "../../assets/projects/sbry.jpg";
 
 const About = () => {
   return (
-    <div name='about' className='w-full h-screen bg-[#0a192f] text-gray-300'>
-     <div className='flex flex-col justify-center items-center w-full h-full'>
-        <div className='max-w-[1000px] w-full grid grid-cols-2 gap-8'>
-          <div className="sm:text-right pb-8 pl-4">
+    <div name="about" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      <div className="flex flex-col justify-center items-center w-full h-full">
+        <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
+          <div className="sm:text-right pb-8 pl-2">
             <p className="text-4xl font-bold inline border-b-4 border-pink-300">
               About
             </p>
           </div>
-          </div>
-          <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
-            <div className="sm:text-right text-4xl font-bold ">
-              <p>
-                Hi! I'm Sabriye, nice to meet you. Please take a look around.
-              </p>
-            </div>
-            <div>
-            <p className='font-bold text-[#8892b0] text-l'> I am passionate about building excellent software that improves
+        </div>
+        <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-2">
+          <div>
+            <p className="text-4xl font-bold mt-11">
+              Hi! I'm Sabriye,<br /> nice to meet you. Please take a look around.
+            </p>
+            <p className=" text-[#8892b0] text-xl mt-11">
+              {" "}
+              I am passionate about building excellent software that improves
               the lives of those around me. I specialize in creating software
               for clients ranging from individuals and small-businesses all the
               way to large enterprise corporations. What would you do if you had
-              a software expert available at your fingertips?</p> 
-            </div>    
+              a software expert available at your fingertips?
+            </p>
+          </div>
+          <img
+        
+            width={350}
+            height={350}
+            className=" shadow-xl shadow-[#698cbe] rounded-full ml-14 sm:block md:w-[400px] md:h-[350px] lg:w-[375px] lg:h-[500px]"
+            alt="about image"
+            style={{ objectFit: "contain" }}
+            src={ProfilFoto} />
         </div>
       </div>
     </div>
